6b85c8ed3e04649d81b87c1daa4473a25967d397
invisibleSascha <sascha@invisibleroom.com>
2021-08-02 14:35:34 +0200

    theme hsb to hex

--- src/components/MaterialController.js
+++ src/components/MaterialController.js
@@ -189,7 +189,53 @@ class MaterialController {
     }
 
     return [h*360, s*100, l*100]
-}
+  }
+
+
+  hexToHSL(H) {
+    // Convert hex to RGB first
+    let r = 0, g = 0, b = 0;
+    if (H.length == 4) {
+      r = "0x" + H[1] + H[1];
+      g = "0x" + H[2] + H[2];
+      b = "0x" + H[3] + H[3];
+    } else if (H.length == 7) {
+      r = "0x" + H[1] + H[2];
+      g = "0x" + H[3] + H[4];
+      b = "0x" + H[5] + H[6];
+    }
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r,g,b),
+        cmax = Math.max(r,g,b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+  
+    if (delta == 0)
+      h = 0;
+    else if (cmax == r)
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      h = (b - r) / delta + 2;
+    else
+      h = (r - g) / delta + 4;
+  
+    h = Math.round(h * 60);
+  
+    if (h < 0)
+      h += 360;
+  
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+  
+    return [h,s,l];
+  } 
 
   lerpColor(arr1, arr2, alpha) {
     var finalArr = [];
@@ -201,8 +247,8 @@ class MaterialController {
     }
 
     for (var i = 0; i < arr1.length; i++) {
-      var val1 = this.hsv_to_hsl(arr1[i].value);
-      var val2 = this.hsv_to_hsl(arr2[i].value);
+      var val1 = this.hexToHSL(arr1[i].value);
+      var val2 = this.hexToHSL(arr2[i].value);
 
       var hsv = this.LerpHSV(val1, val2, alpha);
       //console.log("HSV ",hsv)
