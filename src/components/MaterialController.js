d934c526bc7ebc0ee6fd2de3f45da0619e7ad57a
invisibleSascha <sascha@invisibleroom.com>
2021-08-16 11:55:09 +0200

    fog uniforms

--- src/components/MaterialController.js
+++ src/components/MaterialController.js
@@ -227,29 +227,17 @@ class MaterialController {
     this.tex_bg_back.lerpMaterial(this.gradient_bg_back.GetTexture(), this.gradient_bg_back.GetTexture(), alpha, themeA.tex_bg_back, themeB.tex_bg_back);
 
 
-    console.log("SCENE ", this.xr.Scene);
-
-
+    //Update Fog Uniform Settings
     Object.keys(this.materials).map((matName)=>{
-      
       if(this.materials[matName].hasOwnProperty("uniforms")){
-        
-        
         if(this.materials[matName].uniforms.hasOwnProperty("fogColor")){
-
-          console.log( this.xr.Scene.fog )
-
-          console.log(this.materials[matName].uniforms,this.xr.Scene);
           this.materials[matName].uniforms.fogColor.value = this.xr.Scene.fog.color;
           this.materials[matName].uniforms.fogDensity.value = this.xr.Scene.fog.density;
-          // this.materials[matName].uniforms.fogFar.value = this.xr.Scene.fog.color;
-          // this.materials[matName].uniforms.fogNear.value = this.xr.Scene.fog.color;
+          this.materials[matName].uniforms.fogFar.value = this.xr.Camera.far;
+          this.materials[matName].uniforms.fogNear.value = this.xr.Camera.near;
         }
-
       }
-    })
-    console.log("this " , this.materials);
-
+    });
 
   }
 
